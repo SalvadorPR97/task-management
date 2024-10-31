6251c6fa038f5b933a26dd1eb8867b3d56ed8f80
Salvi <s.perezranchal@gmail.com>
2024-10-31 09:05:40 +0100

    se pueden añadir tareas

--- main.js
+++ main.js
@@ -7,5 +7,28 @@ allDivs[2].className = "li-container";
 allDivs[3].className = "empty";
 allDivs[4].className = "task-count";
 
+const ulTasks = document.querySelector("ul");
+
 const addButton = document.querySelector("button");
 addButton.className = "btn-add";
+
+const txtField = document.querySelector("input");
+
+addButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    anadirTarea(txtField.value);
+})
+
+function anadirTarea(txtTask){
+    const newLi = document.createElement("li");
+    const newP = document.createElement("p");
+    const newSpan = document.createElement("span");
+    newSpan.textContent = txtTask;
+    const closeButton = document.createElement("button");
+    closeButton.className = "btn-delete";
+    closeButton.textContent = "X";
+
+    newP.append(newSpan);
+    newLi.append(newP, closeButton);
+    ulTasks.append(newLi);
+}
