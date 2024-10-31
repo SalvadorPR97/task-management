31058904f4bedccad243946474efe5fd54cc719b
Salvi <s.perezranchal@gmail.com>
2024-10-31 08:49:23 +0100

    añadido elementos html desde js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import './style.css'
+const allDivs = document.querySelectorAll("div");
+
+allDivs[0].className = "container";
+allDivs[1].className = "search";
+allDivs[2].className = "li-container";
+allDivs[3].className = "empty";
+allDivs[4].className = "task-count";
+
+const addButton = document.querySelector("button");
+addButton.className = "btn-add";
